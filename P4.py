e390207dc3799a002e65ebc8a4cefd33f29d0e8e
ALLSEEPANDA <allseepanda@gmail.com>
2024-09-14 20:10:18 +0530

    p

--- P4.py
+++ P4.py
@@ -1,7 +1,9 @@
+# Define variables
 x = 10
 y = 5
 
 # Arithmetic Operations
+print("Arithmetic Operations:")
 print("Addition:", x + y)
 print("Subtraction:", x - y)
 print("Multiplication:", x * y)
@@ -11,6 +13,7 @@ print("Exponentiation:", x ** y)
 print("Floor Division:", x // y)
 
 # Relational Operations
+print("\nRelational Operations:")
 print("Equal:", x == y)
 print("Not Equal:", x != y)
 print("Greater Than:", x > y)
@@ -20,6 +23,7 @@ print("Less Than or Equal:", x <= y)
 
 # Assignment Operations
 z = 15
+print("\nAssignment Operations:")
 print("Initial Value of z:", z)
 z = 15
 z += 5
@@ -57,14 +61,15 @@ print("z after >>= 5:", z)
 z = 10
 z <<= 5
 print("z after <<= 5:", z)
-print("z after := 20:", z := 20)
 
 # Logical Operations
+print("\nLogical Operations:")
 print("And Operation:", x > 3 and x < 10)
 print("Or Operation:", x > 3 or x < 4)
 print("Not Operation:", not(x > 3 and x < 10))
 
 # Bitwise Operations
+print("\nBitwise Operations:")
 print("Bitwise And:", x & y)
 print("Bitwise Or:", x | y)
 print("Bitwise Xor:", x ^ y)
@@ -73,10 +78,12 @@ print("Left Shift:", x << 2)
 print("Right Shift:", x >> 2)
 
 # Ternary Operation
+print("\nTernary Operation:")
 print("Hello" if x < y else "Bye")
 
 # Membership Operations
 a = ["apple", "banana"]
+print("\nMembership Operations:")
 print("Is 'banana' in the list?", "banana" in a)
 print("Is 'pineapple' not in the list?", "pineapple" not in a)
 
@@ -84,6 +91,7 @@ print("Is 'pineapple' not in the list?", "pineapple" not in a)
 x = ["apple", "banana"]
 y = ["apple", "banana"]
 z = x
+print("\nIdentity Operations:")
 print("Is x identical to z?", x is z)
 print("Is x identical to y?", x is y)
 print("Is x not identical to z?", x is not z)
