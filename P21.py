fc1f4a2ecb96f7f418fe529edea4509f0e3716c7
ALLSEEPANDA <allseepanda@gmail.com>
2024-10-27 20:08:27 +0530

    21-50

---
+++ P21.py
@@ -0,0 +1,7 @@
+import sys
+
+tuple1 = ("A", 1, "B", 2, "C", 3)
+tuple2 = ((1, "Lion"), (2, "Tiger"), (3, "Fox"), (4, "Wolf"))
+
+print("Size of Tuple1:", sys.getsizeof(tuple1), "bytes")
+print("Size of Tuple2:", sys.getsizeof(tuple2), "bytes")
